eacd63efcd49c81c49789394761b113bbe6b2307
Hakim <ozil.hakim@gmail.com>
2023-05-02 22:25:55 -0400

    8- pass test

--- portfolio.js
+++ portfolio.js
@@ -11,10 +11,20 @@ class Portfolio {
 
    evaluate(currency) {
       const sum = this.moneys.reduce((acc, money) => {
-         return acc + money.amount;
+         return acc + this.convert(money, currency);
       }, 0);
 
       return new Money(sum, currency);
+   }
+
+   convert(money, currency) {
+      const euroToUsd = 1.2;
+
+      if (money.currency === currency) {
+         return money.amount;
+      }
+
+      return money.amount * euroToUsd;
    }
 }
 
