371ae185ef73bb95d60589bd4f765cec9cd6ed04
Hakim <ozil.hakim@gmail.com>
2023-05-02 21:59:37 -0400

    remove duplicated test

--- money.test.js
+++ money.test.js
@@ -1,17 +1,12 @@
 const { Money } = require("./money");
 const Portfolio = require("./portfolio");
 
-test("dollar multiplication", () => {
+test("test multiplication", () => {
    const money = new Money(5, "USD");
    expect(money.times(2)).toEqual(new Money(10, "USD"));
 });
 
-test("euro multiplication", () => {
-   const money = new Money(10, "EUR");
-   expect(money.times(3)).toEqual(new Money(30, "EUR"));
-});
-
-test("dollar division", () => {
+test("test division", () => {
    const money = new Money(5, "KRW");
    expect(money.divide(2)).toEqual(new Money(2.5, "KRW"));
 });
